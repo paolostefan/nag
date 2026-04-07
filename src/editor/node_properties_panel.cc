08e19d46a7f022c289b2e8633c7d2cc6c41e3c4f
Paolo Stefan <paolostefan@gmail.com>
2026-04-07 15:48:19 +0200

    chore

--- src/editor/node_properties_panel.cc
+++ src/editor/node_properties_panel.cc
@@ -31,9 +31,9 @@ void NodePropertiesPanel::render(NodeGraph &graph, CommandHistory &history) {
   ImGui::PushID("NodePropertiesPanel");
 
   if (selected_count == 0) {
-    ImGui::TextDisabled("No node selected.");
+    ImGui::TextDisabled("No node selected");
   } else if (selected_count > 1) {
-    ImGui::TextDisabled("Multiple nodes selected.");
+    ImGui::TextDisabled("Multiple nodes selected");
   } else {
     // Exactly one node selected.
     if (Node *node = graph.find_node(selected_id); !node) {
